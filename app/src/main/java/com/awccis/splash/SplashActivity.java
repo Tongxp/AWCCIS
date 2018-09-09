8f4e9a22466b36003014e7d99990d4b3572eb389
txp <2687656795@qq.com>
2018-09-09 21:37:33 +0800

    txp 项目初始化

---
+++ app/src/main/java/com/awccis/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.awccis.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.awccis.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
